e29b2bd0a60d7356fc079668b22d01800464f1b2
David Glaeseman <david@avidbrain.com>
2015-10-07 12:05:31 -0700

    mister tutor

--- app/includes/homepage/homepage.include.php
+++ app/includes/homepage/homepage.include.php
@@ -190,20 +190,51 @@
 	</div>
 </div>
 
-
-<div class="container signup-post-a-job">
-	<div class="row">
-		<div class="col s12 m6 l6">
-			<h2>Post A Job</h2>
-			<div class="block">
-				<?php include($app->dependents->APP_PATH.'includes/jobs/postajob.php'); ?>
+<?php if(isset($postajob)): ?>
+	<div class="container signup-post-a-job">
+		<div class="row">
+			<div class="col s12 m6 l6">
+				<h2>Post A Job</h2>
+				<div class="block">
+					<?php include($app->dependents->APP_PATH.'includes/jobs/postajob.php'); ?>
+				</div>
+			</div>
+			<div class="col s12 m6 l6">
+				<h2>Find A Tutor</h2>
+				<div class="block">
+					Show Advanced Find A Tutor
+				</div>
 			</div>
 		</div>
-		<div class="col s12 m6 l6">
-			<h2>Find A Tutor</h2>
-			<div class="block">
-				Show Advanced Find A Tutor
+	</div>
+<?php endif; ?>
+
+<div class="fullsize featured-tutor valign-wrapper">
+	<div class="featured-tutor-text valign">
+		<h2>Featured Tutor</h2>
+	</div>
+	<div class="featured-tutor-container">
+		<div class="featured-tutor-inside">
+
+			<div class="row">
+				<div class="col s12 m4 l4">
+					<div class="profile-image center-align avatar">
+						<img class="avatarbg responsive-img " src="/profiles/approved/--tutors--scottsdale--arizona--glaeseman.crop.jpg" />
+					</div>
+					<div class="featured-tutor-score">
+						<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
+					</div>
+				</div>
+				<div class="col s12 m8 l8">
+					<div class="featured-tutor-name">Mister Tutor</div>
+					<div class="featured-tutor-location">Scottsdale Arizona, 85257</div>
+					<div class="featured-tutor-subjects">Algebra 1, Biology, ACT Science, Martial Arts, River Rafting</div>
+					<div class="featured-tutor-about">
+						Gummi bears fruitcake biscuit chupa chups marzipan powder sesame snaps donut. Tiramisu candy caramels pastry cupcake chupa chups oat cake fruitcake chocolate. Lemon drops halvah pie dessert marzipan caramels chocolate bar croissant. Jelly beans cake gingerbread carrot cake cake. Pie halvah cheesecake pie topping chocolate oat cake lollipop. Cake soufflé brownie tart cupcake. Chocolate bar toffee gummies. Bear claw biscuit icing tootsie roll liquorice cheesecake. Lemon drops  candy canes toffee applicake jelly beans.
+					</div>
+				</div>
 			</div>
+
 		</div>
 	</div>
 </div>
