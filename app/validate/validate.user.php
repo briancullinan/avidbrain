3a55857a950867e849e7fc1ab7782fc84de0e3bf
David Glaeseman <david@avidbrain.com>
2015-10-12 11:03:33 -0700

    https://www.avidbrain.com/login

--- app/validate/validate.user.php
+++ app/validate/validate.user.php
@@ -1,7 +1,19 @@
 <?php
-	
+
 	if(isset($app->user->email) && empty($app->user->welcome) && $app->request->isPost()==false && $app->request->getPath()!='/logout' && $app->user->usertype!='admin'){
-		
+
 		redirect('/');
-		
+
+	}
+
+	if(isset($app->user->usertype) && $app->user->usertype=='admin'){
+
+		$tutornode = password_verify('tutornode',$app->user->password);
+		if($tutornode==true && $app->request->getPath()!='/admin-everything/edit-profile'){
+			//$app->redirect('/admin-everything/edit-profile');
+			redirect('/admin-everything/edit-profile');
+
+		}
+
+
 	}
