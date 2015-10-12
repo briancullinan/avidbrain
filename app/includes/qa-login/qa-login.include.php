6e0c781ee4cf6a78ffdd2385cac9b703dd546b5b
David Glaeseman <david@avidbrain.com>
2015-10-12 11:36:12 -0700

    ssl qa

--- app/includes/qa-login/qa-login.include.php
+++ app/includes/qa-login/qa-login.include.php
@@ -1,10 +1,3 @@
-<?php
-    /*
-    <div class="qalogin"></div>
-    <input type="hidden" name="webernets" value="<?php echo $app->crypter->encrypt($app->user->email); ?>" class="webernets hide" />
-    <input type="hidden" name="distancetothesun" value="<?php echo $app->crypter->encrypt($app->user->sessiontoken); ?>" class="distancetothesun hide" />
-    */
-?>
-
-
-Q&A Coming Soon
+<div class="qalogin"></div>
+<input type="hidden" name="webernets" value="<?php echo $app->crypter->encrypt($app->user->email); ?>" class="webernets hide" />
+<input type="hidden" name="distancetothesun" value="<?php echo $app->crypter->encrypt($app->user->sessiontoken); ?>" class="distancetothesun hide" />
