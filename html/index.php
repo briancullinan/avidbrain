d597ff254e9f58886b12ee4bf1521e0f7f47faa0
David Glaeseman <david@avidbrain.com>
2015-10-12 06:39:24 -0700

    index

--- html/index.php
+++ html/index.php
@@ -30,7 +30,6 @@
 		header('Cache-Control: post-check=0, pre-check=0', false);
 		header('Pragma: no-cache');
 	}
-
 	include($app->dependents->APP_PATH.'functions/global.functions.php');
 	require '../app/autoload/autoload.php';
 
