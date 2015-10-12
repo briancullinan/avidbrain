5cbf22a10e06d2176d011310b044d5207da65ad2
David Glaeseman <david@avidbrain.com>
2015-10-12 06:33:10 -0700

    fixes

--- app/includes/admin-everything/edit-profile/edit-profile.include.php
+++ app/includes/admin-everything/edit-profile/edit-profile.include.php
@@ -1,3 +1,11 @@
+<h2>Change Password</h2>
 
-/admin-everything/edit-profile
-
+<?php
+    $thelogin = new Forms($app->connect);
+    $thelogin->formname = 'changepassword';
+    $thelogin->url = $app->request->getPath();
+    $thelogin->dependents = $app->dependents;
+    $thelogin->csrf_key = $csrf_key;
+    $thelogin->csrf_token = $csrf_token;
+    $thelogin->makeform();
+?>
--- app/includes/admin-everything/edit-profile/edit-profile.post.php
+++ app/includes/admin-everything/edit-profile/edit-profile.post.php
@@ -1,1 +1,22 @@
-<?php notify($app->keyname); ?>
+<?php
+
+    if(isset($app->changepassword)){
+
+        if($app->changepassword->password_new!=$app->changepassword->password_new_confirm){
+            new Flash(array('action'=>'required','formID'=>'changepassword','field'=>'field_changepassword_password_new_confirm','message'=>"Your password doesn't match <i class='fa fa-lock'></i>"));
+        }
+        elseif(strlen($app->changepassword->password_new) < 6){
+            new Flash(array('action'=>'required','message'=>"Passwords must be at least <span>6 characters</span>"));
+        }
+
+        $password = password_hash($app->changepassword->password_new, PASSWORD_DEFAULT);
+        $update = array(
+            'password'=>$password
+        );
+
+        $app->connect->update('avid___admins',$update,array('email'=>$app->user->email));
+
+
+        new Flash(array('action'=>'kill-form','formID'=>'changepassword','message'=>'Password Changed'));
+
+    }
