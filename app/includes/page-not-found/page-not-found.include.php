ab1a7a76d9c1757f70241cabfdbe21d7bdb66c39
David Glaeseman <david@avidbrain.com>
2015-10-12 08:04:31 -0700

    <p>Oh, it looks like you found one of our missing pages. Here is a picture of a cat. </p>
    <?php
    	$kitties = array(
    		'https://www.avidbrain.com/images/kitty.gif',
    		'https://www.avidbrain.com/images/kitty2.gif'
    	);
    	shuffle($kitties);
    	$kitties = $kitties[0];
    ?>
    
    <a href="/"><img class="responsive-img" src="<?php echo $kitties; ?>" /></a>

--- app/includes/page-not-found/page-not-found.include.php
+++ app/includes/page-not-found/page-not-found.include.php
@@ -1,12 +1,24 @@
-<p>Oh, it looks like you found one of our missing pages. Here is a picture of a cat. </p>
-
 <?php
-	$kitties = array(
-		'http://avidbrane.com/images/kitty.gif',
-		'http://avidbrane.com/images/kitty2.gif'
-	);
-	shuffle($kitties);
-	$kitties = $kitties[0];
+
+	/*
+
+	<p>Oh, it looks like you found one of our missing pages. Here is a picture of a cat. </p>
+
+	<?php
+		$kitties = array(
+			'https://www.avidbrain.com/images/kitty.gif',
+			'https://www.avidbrain.com/images/kitty2.gif'
+		);
+		shuffle($kitties);
+		$kitties = $kitties[0];
+	?>
+
+	<a href="/"><img class="responsive-img" src="<?php echo $kitties; ?>" /></a>
+
+
+	*/
+
 ?>
 
-<a href="/"><img class="responsive-img" src="<?php echo $kitties; ?>" /></a>
+
+Uh, oh. You found our 404 Page.
