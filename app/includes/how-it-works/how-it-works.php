fba23b7fa094a98b3cb6814cdc0417a5d399aab6
David Glaeseman <david@avidbrain.com>
2015-10-12 11:27:42 -0700

    fixed mobile images

--- app/includes/how-it-works/how-it-works.php
+++ app/includes/how-it-works/how-it-works.php
@@ -1,63 +1,63 @@
 <div class="how-it-works-slide">
-	
+
 	<div class="close-it-works"><i class="fa fa-times"></i></div>
-	
+
 	<h1>How It Works</h1>
-	
-	
+
+
 	<div class="row">
 		<div class="col s12 m4 l4">
-			
+
 			<div class="howie-howitworks-img">
-				<img class="responsive-img blue" src="/images/how-it-works/search.png" />
+				<img class="responsive-img blue hide-on-small-only" src="/images/how-it-works/search.png" />
 			</div>
-			
+
 			<div class="howie-howitworks-title">
 				Discover Amazing Tutors
 			</div>
-			
+
 			<div class="howie-howitworks-text">
 				Get help in any subject you can think of. We have instructors in both academic and non- academic subjects.
 			</div>
-			
+
 		</div>
 		<div class="col s12 m4 l4">
-			
+
 			<div class="howie-howitworks-img">
-				<img class="responsive-img blue darken-1" src="/images/how-it-works/communicate.png" />
+				<img class="responsive-img blue darken-1 hide-on-small-only" src="/images/how-it-works/communicate.png" />
 			</div>
-			
+
 			<div class="howie-howitworks-title">
 				Hire a Tutor
 			</div>
-			
+
 			<div class="howie-howitworks-text">
-				Connect with tutors, confirm tutoring sessions, and pay - all through AvidBrain’s trusted services. 
+				Connect with tutors, confirm tutoring sessions, and pay - all through AvidBrain’s trusted services.
 			</div>
-			
+
 		</div>
 		<div class="col s12 m4 l4">
-			
+
 			<div class="howie-howitworks-img">
-				<img class="responsive-img blue darken-2" src="/images/how-it-works/whytutor.png" />
+				<img class="responsive-img blue darken-2 hide-on-small-only" src="/images/how-it-works/whytutor.png" />
 			</div>
-			
+
 			<div class="howie-howitworks-title">
 				Learn
 			</div>
-			
+
 			<div class="howie-howitworks-text">
 				See the progress in any subject you choose to learn.
 			</div>
-			
+
 		</div>
 	</div>
-	
+
 	<div class="howie-howitworks-bottom">
 
-		
+
 		<a href="/how-it-works">Learn more</a> about tutoring on <?php echo $app->dependents->SITE_NAME_PROPPER; ?> and show your students how well you can teach them.
-		
+
 	</div>
-	
+
 </div>
