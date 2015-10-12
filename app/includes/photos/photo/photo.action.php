c4c9223e02fb6532fbeafdce18fe4cdfba2d3def
David Glaeseman <david@avidbrain.com>
2015-10-12 13:23:55 -0700

    $isthecrophere

--- app/includes/photos/photo/photo.action.php
+++ app/includes/photos/photo/photo.action.php
@@ -1,21 +1,23 @@
 <?php
 	$usertype = explode('/',$app->request->getPath());
 	$usertype=$usertype[1];
-	
+
 $url = '/'.$usertype.'/'.$state.'/'.$city.'/'.$username;
-	
+
 if(isset($usertype) && isset($state) && isset($city) && isset($username)){
-	
+
 	$urlfix = str_replace('/','--',$url);
 	$file = $app->dependents->DOCUMENT_ROOT.'profiles/approved/'.$urlfix.'*crop*';
 	$isthecrophere = glob($file);
-	
+
 	if(isset($app->user->usertype) && $app->user->usertype=='admin'){
 		$urlfix = str_replace('/','--',$url);
 		$file = $app->dependents->APP_PATH.'uploads/photos/'.$urlfix.'*';
 		$isthecrophere = glob($file);
-		$isthecrophere = str_replace('.crop.','.',$isthecrophere[0]);
-		$img = $app->imagemanager->make($isthecrophere);
+		if(isset($isthecrophere[0])){
+			$isthecrophere = str_replace('.crop.','.',$isthecrophere[0]);
+			$img = $app->imagemanager->make($isthecrophere);
+		}
 	}
 	elseif(isset($app->user->url) && $app->user->url==$url){
 		$img = $app->imagemanager->make($app->user->my_upload);
