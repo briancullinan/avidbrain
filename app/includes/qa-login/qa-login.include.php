43add787acaea86707475d57e1f324c014ec4a0d
David Glaeseman <david@avidbrain.com>
2015-10-12 10:44:32 -0700

    Q&A Coming Soon

--- app/includes/qa-login/qa-login.include.php
+++ app/includes/qa-login/qa-login.include.php
@@ -6,6 +6,5 @@
     */
 ?>
 
-<div class="qalogin"></div>
-<input type="hidden" name="webernets" value="<?php echo $app->crypter->encrypt($app->user->email); ?>" class="webernets hide" />
-<input type="hidden" name="distancetothesun" value="<?php echo $app->crypter->encrypt($app->user->sessiontoken); ?>" class="distancetothesun hide" />
+
+Q&A Coming Soon
