c1a67a90b559910261c55c6221ad7d6d6c81795b
David Glaeseman <david@avidbrain.com>
2015-10-13 15:27:02 -0700

    $app->user->status

--- app/includes/jobs/jobs.student.post.php
+++ app/includes/jobs/jobs.student.post.php
@@ -70,8 +70,8 @@
         $data	=	$data->setParameter(':parent_slug',$app->postjob->parent_slug);
         $data	=	$data->execute()->fetchAll();
 
-        if(isset($data[0]) && $app->dependents->MODE == 'production'){
-            
+        if(isset($data[0]) && empty($app->user->status) && $app->dependents->MODE == 'production'){
+
             $subject = 'A student has posted a new job';
             $message = '<br><h2>'.$app->postjob->subject_name.' Student</h2>';
 
