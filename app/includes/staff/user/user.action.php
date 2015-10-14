6efcb0d8470d96cf1b091c311fb2a9b2044b68cf
David Glaeseman <david@avidbrain.com>
2015-10-14 08:56:41 -0700

    $user

--- app/includes/staff/user/user.action.php
+++ app/includes/staff/user/user.action.php
@@ -1,14 +1,20 @@
 <?php
-	$sql = "SELECT first_name,last_name,my_avatar,short_description,personal_statement FROM avid___admins WHERE url = :url";
-	$prepeare = array(':url'=>$app->request->getPath());
-	$results = $app->connect->executeQuery($sql,$prepeare)->fetch();
 
-	if(isset($results->first_name)){
-		$app->userinfo = $results;
+	if(isset($user)){
+		$sql = "SELECT first_name,last_name,my_avatar,short_description,personal_statement FROM avid___admins WHERE url = :url";
+		$prepeare = array(':url'=>'/staff/'.$user);
+		$results = $app->connect->executeQuery($sql,$prepeare)->fetch();
 
-		$app->meta = new stdClass();
-		$app->meta->title = $results->first_name.' '.$results->last_name.' - '.$app->dependents->SITE_NAME_PROPPER.' '.$results->short_description;
-		$app->meta->h1 = false;
+		if(isset($results->first_name)){
+			$app->userinfo = $results;
+
+			$app->meta = new stdClass();
+			$app->meta->title = $results->first_name.' '.$results->last_name.' - '.$app->dependents->SITE_NAME_PROPPER.' '.$results->short_description;
+			$app->meta->h1 = false;
+		}
+		else{
+			$app->redirect('/staff');
+		}
 	}
 	else{
 		$app->redirect('/staff');
