75845eef74144e9a9f3069d72f589d17bde377cf
David Glaeseman <david@avidbrain.com>
2015-10-14 08:31:15 -0700

    first_name staff fix

--- app/includes/staff/user/user.action.php
+++ app/includes/staff/user/user.action.php
@@ -2,9 +2,14 @@
 	$sql = "SELECT first_name,last_name,my_avatar,short_description,personal_statement FROM avid___admins WHERE url = :url";
 	$prepeare = array(':url'=>$app->request->getPath());
 	$results = $app->connect->executeQuery($sql,$prepeare)->fetch();
-	
-	$app->userinfo = $results;
-	
-	$app->meta = new stdClass();
-	$app->meta->title = $results->first_name.' '.$results->last_name.' - '.$app->dependents->SITE_NAME_PROPPER.' '.$results->short_description;
-	$app->meta->h1 = false;
+
+	if(isset($results->first_name)){
+		$app->userinfo = $results;
+
+		$app->meta = new stdClass();
+		$app->meta->title = $results->first_name.' '.$results->last_name.' - '.$app->dependents->SITE_NAME_PROPPER.' '.$results->short_description;
+		$app->meta->h1 = false;
+	}
+	else{
+		$app->redirect('/staff');
+	}
