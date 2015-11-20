ec420a1e276d3df0a64d93ca0e325d2ea1524fb1
David Glaeseman <david@avidbrain.com>
2015-11-20 11:54:20 -0700

    fixes, small fixes for the view everyone

--- app/includes/admin-everything/view-everyone/view-everyone.action.php
+++ app/includes/admin-everything/view-everyone/view-everyone.action.php
@@ -1,17 +1,18 @@
 <?php
-	
-	$sql = "SELECT * FROM avid___user WHERE usertype = :usertype ORDER BY first_name ASC";
+
+	$sql = "SELECT first_name,last_name,url FROM avid___user WHERE  first_name IS NOT NULL AND url IS NOT NULL  ORDER BY id DESC";//WHERE Length(first_name) > 1
+	//SELECT IFNULL( (SELECT field1 FROM table WHERE id = 123 LIMIT 1) ,'not found');
 	$prepeare = array(':usertype'=>'tutor');
 	$app->viewtutors = $app->connect->executeQuery($sql,$prepeare)->fetchAll();
-	
+	//printer($app->viewtutors,1);
+
 	$sql = "SELECT * FROM avid___user WHERE usertype = :usertype ORDER BY first_name ASC";
 	$prepeare = array(':usertype'=>'student');
 	$app->viewstudents = $app->connect->executeQuery($sql,$prepeare)->fetchAll();
-	
+
 	$sql = "SELECT * FROM avid___users_temp ORDER BY first_name ASC";
 	$prepeare = array();
 	$app->viewpending = $app->connect->executeQuery($sql,$prepeare)->fetchAll();
-	
+
 	$app->meta = new stdClass();
 	$app->meta->title = 'View Everyone';
-
--- app/includes/admin-everything/view-everyone/view-everyone.include.php
+++ app/includes/admin-everything/view-everyone/view-everyone.include.php
@@ -1,36 +1,36 @@
 <div class="box">
 	<form method="post" action="<?php echo $app->request->getPath(); ?>">
-	
+
 		<div class="input-field">
 			<input id="textid" type="text" name="findatutor[search]" class="validate" placeholder="First Name, Last Name, Email" value="<?php if(isset($app->findatutor->search)){ echo $app->findatutor->search;} ?>">
 			<label for="textid">
 				Search for a user
 			</label>
 		</div>
-		
+
 		<input type="hidden" name="findatutor[target]" value="findatutor"  />
 		<input type="hidden" name="<?php echo $csrf_key; ?>" value="<?php echo $csrf_token; ?>">
-		
+
 		<div class="form-submit">
 			<button class="btn blue" type="submit">
 				Search
-			</button>		
+			</button>
 		</div>
-		
+
 	</form>
-	
-	
+
+
 </div>
-	
+
 <div class="block-in-block">
 	<?php if(isset($app->results)): ?>
 		<?php foreach($app->results as $results): ?>
 			<div class="block">
-			
+
 				<div class="title"><a href="<?php echo $results->url; ?>"><?php echo $results->first_name.' '.$results->last_name; ?></a></div>
 				<div><?php echo $results->email; ?></div>
 				<div><?php echo $results->usertype; ?></div>
-			
+
 			</div>
 		<?php endforeach; ?>
 	<?php endif; ?>
@@ -41,7 +41,7 @@
 		<h3>All Tutors</h3>
 		<?php if(isset($app->viewtutors[0])): ?>
 			<?php foreach($app->viewtutors as $value): ?>
-				<div><a href="<?php echo $value->url; ?>"><?php echo the_users_name($value); ?></a></div>
+				<div><a href="<?php echo $value->url; ?>"> -- <?php echo $value->first_name.' '.$value->last_name; ?></a></div>
 			<?php endforeach; ?>
 		<?php else: ?>
 			There are no tutors
