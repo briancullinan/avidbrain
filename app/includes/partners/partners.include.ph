b035be9ee882663a18bced04824f05288ab25350
David Glaeseman <david@avidbrain.com>
2015-11-20 08:51:14 -0700

    partners

--- app/includes/partners/partners.include.ph
+++
@@ -1,9 +0,0 @@
-Friends of The Library
-http://www.plfriends.org/
-
-
-Arizona Aquatic Sports Foundation
-http://www.azh2ofund.com/
-
-Knights of Heroes
-http://knightsofheroes.org/
--- app/includes/partners/partners.include.php
+++ app/includes/partners/partners.include.php
@@ -1,49 +1,61 @@
 <div class="row">
 	<div class="col s12 m6 l6">
-		some workds about partners and stuffs
+		Gummi bears fruitcake biscuit chupa chups marzipan powder sesame snaps donut. Tiramisu candy caramels pastry cupcake chupa chups oat cake fruitcake chocolate. Lemon drops halvah pie dessert marzipan caramels chocolate bar croissant. Jelly beans cake gingerbread carrot cake cake. Pie halvah cheesecake pie topping chocolate oat cake lollipop. Cake soufflé brownie tart cupcake. Chocolate bar toffee gummies. Bear claw biscuit icing tootsie roll liquorice cheesecake. Lemon drops  candy canes toffee applicake jelly beans.
 	</div>
 	<div class="col s12 m6 l6">
         <ul class="collection">
         	<li class="collection-item">
         		<div class="row">
         			<div class="col s1 m1 l1">
-        				<i class="fa fa-star green-text"></i>
+        				<i class="fa fa-link green-text"></i>
         			</div>
         			<div class="col s11 m11 l11">
-        				collection-item
+        				<a href="http://www.plfriends.org/" target="_blank">
+							Friends of The Library
+						</a>
         			</div>
         		</div>
         	</li>
         	<li class="collection-item">
         		<div class="row">
         			<div class="col s1 m1 l1">
-        				<i class="fa fa-star green-text"></i>
+        				<i class="fa fa-link green-text"></i>
         			</div>
         			<div class="col s11 m11 l11">
-        				collection-item
+        				<a href="http://www.azh2ofund.com/" target="_blank">
+							Arizona Aquatic Sports Foundation
+						</a>
         			</div>
         		</div>
         	</li>
         	<li class="collection-item">
         		<div class="row">
         			<div class="col s1 m1 l1">
-        				<i class="fa fa-star green-text"></i>
+        				<i class="fa fa-link green-text"></i>
         			</div>
         			<div class="col s11 m11 l11">
-        				collection-item
+						<a href="http://knightsofheroes.org/" target="_blank">
+							Knights of Heroes
+						</a>
         			</div>
         		</div>
         	</li>
-        	<li class="collection-item">
-        		<div class="row">
-        			<div class="col s1 m1 l1">
-        				<i class="fa fa-star green-text"></i>
-        			</div>
-        			<div class="col s11 m11 l11">
-        				collection-item
-        			</div>
-        		</div>
-        	</li>
+        	<?php
+			/*
+			<li class="collection-item">
+				<div class="row">
+					<div class="col s1 m1 l1">
+						<i class="fa fa-link green-text"></i>
+					</div>
+					<div class="col s11 m11 l11">
+						<a href="xxx" target="_blank">
+							xxx
+						</a>
+					</div>
+				</div>
+			</li>
+			*/
+			?>
         </ul>
 	</div>
 </div>
