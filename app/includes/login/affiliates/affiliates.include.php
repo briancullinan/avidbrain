941005470330028c95efb5ed20d067d9ae6fcadc
David Glaeseman <david@avidbrain.com>
2015-12-03 15:16:49 -0700

    deals

--- app/includes/login/affiliates/affiliates.include.php
+++ app/includes/login/affiliates/affiliates.include.php
@@ -1,10 +1,21 @@
-<?php
-    $thelogin = new Forms($app->connect);
-    $thelogin->formname = 'login';
-    $thelogin->url = '/login/affiliates';
-    $thelogin->dependents = $app->dependents;
-    $thelogin->csrf_key = $csrf_key;
-    $thelogin->csrf_token = $csrf_token;
-    $thelogin->button = 'Login';
-    $thelogin->makeform();
-?>
+<div class="row">
+	<div class="col s12 m6 l6">
+        <?php
+            $thelogin = new Forms($app->connect);
+            $thelogin->formname = 'login';
+            $thelogin->url = '/login/affiliates';
+            $thelogin->dependents = $app->dependents;
+            $thelogin->csrf_key = $csrf_key;
+            $thelogin->csrf_token = $csrf_token;
+            $thelogin->button = 'Login';
+            $thelogin->makeform();
+        ?>
+
+	</div>
+	<div class="col s12 m6 l6">
+		<p>Don't have an affiliate account? Signup now, it's fast and it's easy.</p>
+
+        <a class="btn blue" href="/signup/affiliate">Affiliate Signup</a>
+
+	</div>
+</div>
