7a9423878050f4cf33d83996f94fb34e800a6f0d
David Glaeseman <david@avidbrain.com>
2015-12-08 10:30:58 -0700

    fix

--- app/includes/admin-everything/new-tutor-approvals/new-tutor-approvals.action.php
+++ app/includes/admin-everything/new-tutor-approvals/new-tutor-approvals.action.php
@@ -7,21 +7,21 @@
         $app->everyoneelse = $results;
     }
 
-    $sql = "SELECT first_name,last_name,email,id FROM avid___new_temps WHERE complete IS NOT NULL AND  activated IS NULL AND approval_status IS NULL ORDER BY id ASC";
+    $sql = "SELECT first_name,last_name,email,id FROM avid___new_temps WHERE complete IS NOT NULL AND  activated IS NULL AND approval_status IS NULL  ORDER BY ID DESC ";
     $prepare = array();
     $results = $app->connect->executeQuery($sql,$prepare)->fetchAll();
     if(isset($results[0])){
         $app->newtutors = $results;
     }
 
-    $sql = "SELECT first_name,last_name,email,id FROM avid___new_temps WHERE  approval_status = 'approved' ORDER BY id DESC";
+    $sql = "SELECT first_name,last_name,email,id FROM avid___new_temps WHERE  approval_status = 'approved'  ORDER BY ID DESC ";
     $prepare = array();
     $results = $app->connect->executeQuery($sql,$prepare)->fetchAll();
     if(isset($results[0])){
         $app->approvedtutors = $results;
     }
 
-    $sql = "SELECT first_name,last_name,email,id FROM avid___new_temps WHERE complete IS NOT NULL AND  activated IS NULL AND approval_status = 'rejected' ORDER BY id ASC";
+    $sql = "SELECT first_name,last_name,email,id FROM avid___new_temps WHERE complete IS NOT NULL AND  activated IS NULL AND approval_status = 'rejected'  ORDER BY ID DESC ";
     $prepare = array();
     $results = $app->connect->executeQuery($sql,$prepare)->fetchAll();
     if(isset($results[0])){
