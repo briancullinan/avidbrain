9121c3bbc0558349b6d5930d0710215c27b08a99
David Glaeseman <david@avidbrain.com>
2016-01-25 17:49:14 -0700

    ial

--- app/includes/confirmation/confirmation.email-signup.php
+++ app/includes/confirmation/confirmation.email-signup.php
@@ -1,1 +1,21 @@
 Thank you for entering your email address, we will contact you shortly
+
+
+<!-- Google Code for Student Sign Up Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 945094692;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "ZwOUCO_lrmMQpIDUwgM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/945094692/?label=ZwOUCO_lrmMQpIDUwgM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
