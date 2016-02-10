87c6d8406516982dc51252616e28e804d43f3502
David Glaeseman <contact@davidglaeseman.com>
2016-02-10 10:51:57 -0700

    Added more info on affiliates

--- app/includes/signup/affiliate/affiliate.action.php
+++ app/includes/signup/affiliate/affiliate.action.php
@@ -2,6 +2,6 @@
     //$app->redirect('/signup');
     $app->meta = new stdClass();
     $app->meta->title = 'AvidBrain Affiliate Program';
-    $app->meta->h1 = 'Affiliate Program';
+    $app->meta->h1 = 'Become an AvidBrain affiliate';
     $app->meta->keywords = 'earn money for every student, or tutor that signs up';
     $app->meta->description = 'affiliate,avid,brain,avidbrain';
--- app/includes/signup/affiliate/affiliate.include.php
+++ app/includes/signup/affiliate/affiliate.include.php
@@ -2,69 +2,93 @@
 	<div class="row">
 		<div class="col s12 m6 l8">
 
-			<div class="row">
-				<div class="col s12 m12 l7">
-					<div class="affiliate-text">
-						<p>Whatever walk of life you are in there are several ways anyone can advertise for AvidBrain and make a some extra cash.</p>
+			<div><img class="responsive-img" src="/images/affiliate.jpg" /></div>
+			<div class="affiliate-text">
+				<p>Whatever walk of life you are in there are several ways anyone can advertise for AvidBrain and make a some extra cash.</p>
+				<p>Become an AvidBrain affiliate and earn $20 for every student or tutor who signs up and completes a session.</p>
+				<p>Earn Part-time Income By Doing What you Love.</p>
+			</div>
 
-						<p>Become an AvidBrain affiliate and earn $20 for every student or tutor who signs up and completes a session.</p>
+			<div class="row affbens">
+
+				<div class="col s12 m12 l4 center-align">
+					<div class="block">
+
+						<div class="affbens-title">
+							Signup
+						</div>
+
+						<div class="afbens-icon">
+							<div class="affbens-stack">
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x green-text"></i>
+									<i class="fa fa-pencil fa-stack-1x white-text"></i>
+								</span>
+							</div>
+							<div class="affbens-drop">
+								<i class="fa fa-sort-down"></i>
+							</div>
+						</div>
+
+						<div class="affbens-text">
+							Signup now and get a unique promo code that you can share, post, blog or do whatever you can think of with.
+						</div>
+
 					</div>
+				</div>
 
+				<div class="col s12 m12 l4 center-align">
+					<div class="block">
+
+						<div class="affbens-title">
+							Download
+						</div>
+
+						<div class="afbens-icon">
+							<div class="affbens-stack">
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x green-text"></i>
+									<i class="fa fa-download fa-stack-1x white-text"></i>
+								</span>
+							</div>
+							<div class="affbens-drop">
+								<i class="fa fa-sort-down"></i>
+							</div>
+						</div>
+
+						<div class="affbens-text">
+							Download our Flyers and Start sharing on your social media network or share your promo code with friends and colleagues.
+						</div>
+
+					</div>
 				</div>
-				<div class="col s12 m12 l5">
-					<ul class="collection">
-				        	<li class="collection-item">
-				        		<div class="row">
-				        			<div class="col s1 m1 l1">
-				        				<i class="fa fa-check green-text"></i>
-				        			</div>
-				        			<div class="col s11 m11 l11">
-				        				Earn easy money
-				        			</div>
-				        		</div>
-				        	</li>
-							<li class="collection-item">
-				        		<div class="row">
-				        			<div class="col s1 m1 l1">
-				        				<i class="fa fa-check green-text"></i>
-				        			</div>
-				        			<div class="col s11 m11 l11">
-				        				Track signups
-				        			</div>
-				        		</div>
-				        	</li>
-							<li class="collection-item">
-				        		<div class="row">
-				        			<div class="col s1 m1 l1">
-				        				<i class="fa fa-check green-text"></i>
-				        			</div>
-				        			<div class="col s11 m11 l11">
-				        				Work when and where you want
-				        			</div>
-				        		</div>
-				        	</li>
-							<li class="collection-item">
-				        		<div class="row">
-				        			<div class="col s1 m1 l1">
-				        				<i class="fa fa-check green-text"></i>
-				        			</div>
-				        			<div class="col s11 m11 l11">
-				        				Be your own boss
-				        			</div>
-				        		</div>
-				        	</li>
-							<li class="collection-item">
-				        		<div class="row">
-				        			<div class="col s1 m1 l1">
-				        				<i class="fa fa-check green-text"></i>
-				        			</div>
-				        			<div class="col s11 m11 l11">
-				        				Sit back &amp; relax
-				        			</div>
-				        		</div>
-				        	</li>
-				        </ul>
+
+				<div class="col s12 m12 l4 center-align">
+					<div class="block">
+
+						<div class="affbens-title">
+							Make Money
+						</div>
+
+						<div class="afbens-icon">
+							<div class="affbens-stack">
+								<span class="fa-stack fa-lg">
+									<i class="fa fa-circle fa-stack-2x green-text"></i>
+									<i class="fa fa-dollar fa-stack-1x white-text"></i>
+								</span>
+							</div>
+							<div class="affbens-drop">
+								<i class="fa fa-sort-down"></i>
+							</div>
+						</div>
+
+						<div class="affbens-text">
+							Start making passive income with our easy to use dashboard &amp; keep track of your earnings.
+						</div>
+
+					</div>
 				</div>
+
 			</div>
 
 
@@ -98,15 +122,72 @@
 
 			</div>
 
+			<!---->
+			<h3 class="white-text">Affiliate Benefits</h3>
+			<ul class="collection collection-fix">
+				<li class="collection-item">
+					Make great money working part-time
+				</li>
+				<li class="collection-item">
+					Work from the convenient of your home
+				</li>
+				<li class="collection-item">
+					Every tutor or student that you get to signup on our site and that have a session, you earn $20!
+				</li>
+				<li class="collection-item">
+					Easy to promote, every student that signups with your PROMO CODE get $30 off their first session
+				</li>
+				<li class="collection-item">
+					Every tutors that signups up with your PROMO CODE starts at 70% take-home rate. Highest in the Industry
+				</li>
+				<li class="collection-item">
+					It is free for students and tutors to signup
+				</li>
 
+		    </ul>
+			<!---->
 
 		</div>
+
 	</div>
 
 </div>
 
 
+
 <style type="text/css">
+.collection-fix{
+	float: left;
+	width: 100%;
+}
+.affbens-title{
+
+}
+.affbens-title{
+	font-family: 'Quicksand';
+	font-weight: 400;
+	font-size: 30px;
+	margin-bottom: 10px;
+}
+.affbens-stack{
+	font-size: 30px;
+	position: relative;
+	z-index: 11;
+}
+.affbens-drop{
+	font-size: 75px;
+	margin-top: -77px;
+	color: #efefef;
+	position: relative;
+	z-index: 10;
+	margin-bottom: -10px;
+}
+.affbens-text{
+	font-size: 13px;
+}
+.new-signup .affbens .block{
+	min-height: 270px;
+}
 #affsignup{
 	padding: 0px;
 }
