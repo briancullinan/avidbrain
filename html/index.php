3619952a06c0a6b65dece40d041e59b79dfa11ed
Brian Cullinan <megamindbrian@gmail.com>
2017-03-13 12:53:18 -0700

    no message

--- html/index.php
+++ html/index.php
@@ -3,6 +3,8 @@
 	header('Content-Type: text/html; charset=utf-8');
 	mb_internal_encoding("UTF-8");
 	mb_http_output('UTF-8');
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     ini_set( 'upload_max_filesize', '25M');
     ini_set( 'post_max_size', '25M');
 
